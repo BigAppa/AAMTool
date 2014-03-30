039be073da6ad727e9825bd045bb09a7e038ed01
Syed Asad Rahman <s9asad@gmail.com>
2014-03-30 13:07:52 +0100

    updates the mapping

--- cases/aamtool_mapping/map.sh
+++ cases/aamtool_mapping/map.sh
@@ -1,7 +1,9 @@
 
+bin="../../v1.3/"
+rxn="../rxn/balanced/"
 
-for i in ../rxn/balanced/*.rxn
+for i in "$rxn"*.rxn
 do
-java -jar ../../v1.3/AAMTool.jar -Q RXN -q $i -j AAM -f XML -g
+java -jar "$bin"AAMTool.jar -Q RXN -q $i -j AAM -f XML -g
 echo $i
 done
